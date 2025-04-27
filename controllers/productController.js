6dea7a5247119c4f8c83d67b4b80e087f4fd9e7b
sumudusan <ss.sumuduaberathne@gmail.com>
2025-04-27 21:52:13 +0400

    create deleteProduct function

--- controllers/productController.js
+++ controllers/productController.js
@@ -76,3 +76,27 @@ export async function getProduct(req,res){
     }
    
 }
+
+export function deleteProduct(req,res){
+
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message: "Please login as a administrator to delete a product"
+        })
+        return
+    }
+
+    const productId = req.params.productId
+
+    Product.deleteOne(
+        {productId : productId}
+    ).then(()=>{
+        res.json({
+            message : "Product Deleted"
+        })
+    }).catch((error)=>{
+        res.status(403).json({
+            message : error
+        })
+    })
+}
--- routes/productrouter.js
+++ routes/productrouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createProduct, getProduct } from "../controllers/productController.js";
+import  { createProduct, deleteProduct, getProduct } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.post("/", createProduct)
 productRouter.get("/", getProduct)
+productRouter.delete("/", deleteProduct)
 
 export default productRouter;
