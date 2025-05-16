7435ac18e6ac79b847eb9b5681d8053bce6bfb26
sumudusan <ss.sumuduaberathne@gmail.com>
2025-05-16 21:15:16 +0400

    create googleLogin function.✅

--- routes/userrouter.js
+++ routes/userrouter.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createUser, getUsers, loginUser } from '../controllers/userController.js'
+import { createUser, getUsers, googleLogin, loginUser } from '../controllers/userController.js'
 
 const userRouter = express.Router();
 
