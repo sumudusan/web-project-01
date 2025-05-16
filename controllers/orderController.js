1185367fe6426605731852a58d623839bab74e4d
sumudusan <ss.sumuduaberathne@gmail.com>
2025-05-16 12:06:28 +0400

    Fixed some issues.👍

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,48 +1,79 @@
 import Order from "../models/order.js";
 import Product from "../models/product.js";
-import { isCustomer } from "./userController.js";
+import {isAdmin, isCustomer } from "./userController.js";
 
 export async function createOrder(req,res){
-    if(!isCustomer){
+
+    if(!isCustomer(req)){
+
         res.json({
-        message : "Please login as customer to create orders"
+            message : "Please login as customer to create order"
         })
+        return;
     }
-
-    //take the latest product id
-
     
-
+    //take the latest product Id
     try{
-        const latestOrder = await Order.find().sort({date : -1}).limit(1)
 
-    let orderId
-
-    if(latestOrder.length == 0){
-        orderId = "CBC0001"
-    }else{
-        const currentOrderId = latestOrder[0].orderId
+        const latestOrder = await Order.find().sort({orderId : -1}).limit(1)
+        console.log(latestOrder)
         
-        const numberString = currentOrderId.replace("CBC", "")
+        let orderId;
 
-        const number = parseInt(numberString)
+        if(latestOrder.length == 0){
+            orderId = "CBC0001";
+        }else{
 
-        const newNumber = (number+ 1).toString().padStart(4, "0");
+            const currentOrderId = latestOrder[0].orderId
+            const numberString = currentOrderId.replace("CBC","")
+            const number = parseInt(numberString)
+            const newNumber = (number + 1).toString().padStart(4, "0")
 
-        orderId = "CBC" + newNumber
-    }
+            orderId = "CBC" + newNumber;
+            
+        }
+        const newOrderData = req.body
 
-    const newOrderData = req.body
-    newOrderData.orderId = orderId
-    newOrderData.email = req.user.email
+        const newProductArray = []
 
-    const order = new Order(newOrderData)
+        for(let i=0; i<newOrderData.orderedItems.length; i++){
+            
+            const product = await Product.findOne({
+                productId : newOrderData.orderedItems[i].productId
+            })
 
-    await order.save()
+            
+            if(product == null){
+                res.json({
+                    message : "Product with id "+newOrderData.orderedItems[i].productId+" not found"
+                })
+                return
+            }
 
-    res.json({
-        message : "Order created"
-    })
+            newProductArray[i] = {
+                name : product.productName,
+                price : product.lastPrice,
+                quantity : newOrderData.orderedItems[i].qty,
+                image : product.images[0]
+            }
+
+        }
+        console.log(newProductArray)
+
+        newOrderData.orderedItems = newProductArray
+
+        newOrderData.orderId = orderId
+        newOrderData.email = req.user.email
+
+        const order = new Order(newOrderData)
+
+        const savedOrder = await order.save()
+
+        res.json({
+            message : "Order created",
+            order : savedOrder
+        })
+
 
     }catch(error){
         res.status(500).json({
@@ -51,19 +82,26 @@ export async function createOrder(req,res){
     }
 }
 
-export async function getOrders(req, res){
-
+export async function getOrders(req,res){
     try{
-        const orderList = await Order.find()
-
+    if(isCustomer(req)){
+        const orders = await Order.find({email : req.user.email})
+        res.json(orders)
+        return;
+    }else if(isAdmin(req)){
+        const orders = await Order.find({});
+        res.json(orders)
+        return;
+    }else{
         res.json({
-            list : orderList 
-        })
-    }catch(e){
-        res.json({
-            message : "Error"
+            message : "Please login to view orders"
         })
     }
+ }catch(error){
+    res.status(500).json({
+        message : error.message,
+    })
+ }
 }
 
 export async function getQuote(req, res) {
