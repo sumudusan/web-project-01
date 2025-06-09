66067166a7d7516ff762fb79a12c8807dac76256
sumudusan <ss.sumuduaberathne@gmail.com>
2025-06-09 13:36:03 +0400

    add cart attribute.✅

--- models/user.js
+++ models/user.js
@@ -1,42 +1,44 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    email:{
-        type:String,
-        required :true,
-        unique:true
-    },
-    firstName:{
-        type:String,
-        required: true
-    },
-    lastName:{
-        type:String,
-        required:true
-    },
-    isBlocked: {
-        type: Boolean,
-        default : false
-    },
-    type:{
-        type:String,
-        default:"customer"
-    },
-    //profilePicture :{
-      //  type : String,
-        //default : " "
-    //},
-    password:{
-        type:String,
-        required:true
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  isBlocked: {
+    type: Boolean,
+    default: false
+  },
+  type: {
+    type: String,
+    default: "customer"
+  },
+  password: {
+    type: String,
+    required: true
+  },
+
+  // ✅ Add this block:
+  cart: [
+    {
+      productId: String,
+      qty: Number
     }
+  ]
+});
 
-})
-
-const User = mongoose.model("users", userSchema)
+const User = mongoose.model("users", userSchema);
 
 export default User;
-
 //admin
 // "email": "aaa@gmail.com",
 // "password": "123"
